e76835c36663db3403df98cbb77d5d7a535cdf9b
William Rhodes <wtrhodes02@gmail.com>
2025-12-15 21:02:09 -0600

    Landing Page UI and Footer

--- apps/portfolio/src/app/page.tsx
+++ apps/portfolio/src/app/page.tsx
@@ -1,12 +1,18 @@
 import Hero from "@/sections/landing/Hero";
 import FeaturedProjects from "@/sections/landing/FeaturedProjects";
+import SkillsAndTechnologies from "@/sections/landing/SkillsAndTechnologies";
+import EngineeringJourney from "@/sections/landing/EngineeringJourney";
+import Connect from "@/sections/landing/Connect";
 import { Box } from "@mui/material";
 
 export default function Home() {
   return (
-    <Box sx={{ width: "100%", height: "100vh" }}>
+    <Box sx={{ width: "100%" }}>
       <Hero />
       <FeaturedProjects />
+      <SkillsAndTechnologies />
+      <EngineeringJourney />
+      <Connect />
     </Box>
   );
 }
